b53fc752457061ba942fb9249444f99b11bf29c8
Matthew Blackwell <m.blackwell@rochester.edu>
2013-04-20 23:33:31 -0400

    Modified help text

--- R/causalsens.R
+++ R/causalsens.R
@@ -1,3 +1,65 @@
+##' Experimental data from the job training program first studied by LaLonde (1986)
+##'
+##' A dataset of units in an experimental evaluation of a job training
+##' program. Subset to those units with two years of pre-treatment income data.
+##'
+##'
+##' \itemize{
+##'   \item \code{age} - age in years.
+##'   \item \code{education} - number of years of schooling.
+##'   \item \code{black} - 1 if black, 0 otherwise.
+##'   \item \code{hispanic} - 1 if Hispanic, 0 otherwise.
+##'   \item \code{married} - 1 if married, 0 otherwise.
+##'   \item \code{nodegree} - 1 if no high school degree, 0 otherwise.
+##'   \item \code{re74} - earnings ($) in 1974.
+##'   \item \code{re75} - earnings ($) in 1975.
+##'   \item \code{re78} - earnings ($) in 1978.
+##'   \item \code{u74} - 1 if unemployed in 1974, 0 otherwise.
+##'   \item \code{u75} - 1 if unemployed in 1975, 0 otherwise.
+##'   \item \code{treat} - 1 if treated, 0 otherwise.
+##' }
+##'
+##' @docType data
+##' @keywords datasets
+##' @name lalonde.exp
+##' @usage data(lalonde.exp)
+##' @format A data frame with 445 rows and 12 variables
+##' @references LaLonde, Robert J. (1986). Evaluating the Econometric
+##' Evaluations of Training Programs with Experimental Data. The
+##' American Economic Review, 76(4), 604--620.
+NULL
+
+##' Non-experimental data from Lalonde (1986)
+##'
+##' A dataset of experimental treated units and non-experimental
+##' control units from the Panel Study of Income Dynamics (PSID).
+##'
+##' \itemize{
+##'   \item \code{age} - age in years.
+##'   \item \code{education} - number of years of schooling.
+##'   \item \code{black} - 1 if black, 0 otherwise.
+##'   \item \code{hispanic} - 1 if Hispanic, 0 otherwise.
+##'   \item \code{married} - 1 if married, 0 otherwise.
+##'   \item \code{nodegree} - 1 if no high school degree, 0 otherwise.
+##'   \item \code{re74} - earnings ($) in 1974.
+##'   \item \code{re75} - earnings ($) in 1975.
+##'   \item \code{re78} - earnings ($) in 1978.
+##'   \item \code{u74} - 1 if unemployed in 1974, 0 otherwise.
+##'   \item \code{u75} - 1 if unemployed in 1975, 0 otherwise.
+##'   \item \code{treat} - 1 if treated, 0 otherwise.
+##' }
+##'
+##' @docType data
+##' @keywords datasets
+##' @name lalonde.psid
+##' @usage data(lalonde.psid)
+##' @format A data frame with 2675 rows and 12 variables
+##' @references LaLonde, Robert J. (1986). Evaluating the Econometric
+##' Evaluations of Training Programs with Experimental Data. The
+##' American Economic Review, 76(4), 604--620.
+NULL
+
+
 ##' Calculate sensitivity of causal estimates to unmeasured confounding.
 ##'
 ##' This function performs a sensitivity analysis of causal effects
